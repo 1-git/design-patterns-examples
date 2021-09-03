be3824421766fc7248f52e8608fc72095f751516
1-git <one-git@yandex.com>
2021-09-03 21:54:34 +0300

    Strategy

--- behavioral/strategy.php
+++ behavioral/strategy.php
@@ -2,7 +2,10 @@
 
 namespace DesignPattern\Behavioral\Strategy;
 
-interface OpenFileStrategyInterface {
+use Exception;
+
+interface OpenFileStrategyInterface
+{
     public function getFileContent(): string;
 }
 
@@ -11,7 +14,7 @@ class CsvFileOpener implements OpenFileStrategyInterface
     public function getFileContent(): string
     {
         //Open csv
-        return 'Text';
+        return 'Csv content';
     }
 }
 
@@ -20,26 +23,100 @@ class TextFileOpener implements OpenFileStrategyInterface
     public function getFileContent(): string
     {
         //Open text
-        return 'Text';
+        return 'Txt content';
     }
 }
 
-class Content{
+###########################################################
+####### SOLUTION 1 - STRATEGY DETECT OUTSIDE OF CLASS #####
+###########################################################
+
+class Content1
+{
     protected OpenFileStrategyInterface $openFileStrategy;
 
-    public function __(OpenFileStrategyInterface $openFileStrategy)
+    public function __construct(OpenFileStrategyInterface $openFileStrategy)
     {
         $this->openFileStrategy = $openFileStrategy;
     }
 
-    public function getInfo(): string
+    public function getInfo(string $filename): string
     {
         return $this->openFileStrategy->getFileContent();
     }
 }
 
 $filename = 'one_file.csv';
-$content = new Content(new CsvFileOpener);
+$content = (new Content1(new CsvFileOpener()))->getInfo($filename);
+var_dump($content);
 
 $filename = 'one_file.txt';
-$content = new Content(new TextFileOpener);
+$content = (new Content1(new TextFileOpener()))->getInfo($filename);
+var_dump($content);
+
+###########################################################
+##### SOLUTION 2 - STRATEGY DETECT INSIDE CONTEXT FILE ####
+###########################################################
+
+class Content2
+{
+    protected OpenFileStrategyInterface $openFileStrategy;
+
+    public function getInfo(string $filename): string
+    {
+        $extension = pathinfo($filename)['extension'];
+        if ($extension === 'csv') {
+            $fileOpener = new CsvFileOpener();
+        } elseif ($extension === 'txt') {
+            $fileOpener = new TextFileOpener();
+        }
+
+        return $fileOpener->getFileContent($filename);
+    }
+}
+
+$filename = 'one_file.csv';
+$content = (new Content2())->getInfo($filename);
+var_dump($content);
+
+$filename = 'one_file.txt';
+$content = (new Content2())->getInfo($filename);
+var_dump($content);
+
+###########################################################
+####### SOLUTION 3 - STRATEGY DETECT IN SEPARATE FILE #####
+###########################################################
+
+class Content3
+{
+    public function getInfo(string $filename): string
+    {
+        $handler = (new OpenerFileRouter())->getHandler($filename);
+        return $handler->getFileContent($filename);
+    }
+}
+
+class OpenerFileRouter
+{
+    public function getHandler(string $filename): OpenFileStrategyInterface
+    {
+        $extension = pathinfo($filename)['extension'];
+        if ($extension === 'csv') {
+            $handler = new CsvFileOpener();
+        } elseif ($extension === 'txt') {
+            $handler = new TextFileOpener();
+        } else {
+            throw new Exception();
+        }
+
+        return $handler;
+    }
+}
+
+$filename = 'one_file.csv';
+$content = (new Content3())->getInfo($filename);
+var_dump($content);
+
+$filename = 'one_file.txt';
+$content = (new Content3())->getInfo($filename);
+var_dump($content);
