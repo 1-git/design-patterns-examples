293c40c94daf514b1edd4a8e3d6cb7c2f4d01d4b
1-git <one-git@yandex.com>
2021-09-03 15:36:07 +0300

    Strategy

---
+++ behavioral/strategy.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace DesignPattern\Behavioral\Strategy;
+
+use ArrayIterator;
+use Iterator;
+use IteratorAggregate;
+
+interface OpenFileStrategyInterface {
+    public function getFileContent(): string;
+}
+class CsvFileOpener implements OpenFileStrategyInterface
+{
+    public function getFileContent(): string
+    {
+        //Open csv
+        return 'Text';
+    }
+}
+
+class TextFileOpener implements OpenFileStrategyInterface
+{
+    public function getFileContent(): string
+    {
+        //Open text
+        return 'Text';
+    }
+}
+
+class Content{
+    protected OpenFileStrategyInterface $openFileStrategy;
+
+    public function __(OpenFileStrategyInterface $openFileStrategy)
+    {
+        $this->openFileStrategy = $openFileStrategy;
+    }
+
+    public function getInfo(): string
+    {
+        return $this->openFileStrategy->getFileContent();
+    }
+}
+
+$filename = 'one_file.csv';
+$content = new Content(new CsvFileOpener);
+
+$filename = 'one_file.txt';
+$content = new Content(new TextFileOpener);
