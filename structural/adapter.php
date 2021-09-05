7342e4451c39a16ce4cf7cb16808cb4ed0ac27d0
1-git <one-git@yandex.com>
2021-09-05 22:19:55 +0300

    Adapter

---
+++ structural/adapter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DesignPattern\Behavioral\Adapter;
+
+interface Runnable {
+    public function run();
+}
+
+class Module {
+    public function execute()
+    {
+        var_dump(111);
+    }
+}
+
+class Client {
+    public function handle(Runnable $runnable)
+    {
+        $runnable->run();
+    }
+}
+
+class NewModule implements Runnable {
+    public function run()
+    {
+        (new Module)->execute();
+    }
+}
+
+(new Client())->handle(new NewModule());
