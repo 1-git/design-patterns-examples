86fbe090386804d54c25b6f3f618d46b2429ec4d
1-git <one-git@yandex.com>
2021-09-05 22:12:27 +0300

    Facade

---
+++ structural/facade.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DesignPattern\Behavioral\Facade;
+
+class ModuleA
+{
+    public function actionA()
+    {
+    }
+}
+
+class ModuleB
+{
+    public function execute()
+    {
+    }
+}
+
+
+class ModuleC
+{
+    public function run()
+    {
+    }
+}
+
+class Client {
+    public function handle()
+    {
+        return (new Facade())->getData();
+    }
+}
+
+class Facade {
+    public function getData()
+    {
+        (new ModuleA())->actionA();
+        (new ModuleB())->execute();
+        (new ModuleC())->run();
+    }
+}
+
+(new Client())->handle();
