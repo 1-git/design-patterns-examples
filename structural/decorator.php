4175b8c0d1960d6275b2666cd6c2bcdc83654e2d
1-git <one-git@yandex.com>
2021-09-05 23:01:03 +0300

    Decorator

---
+++ structural/decorator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DesignPattern\Behavioral\Decorator;
+
+class Example
+{
+    public function run()
+    {
+        return 111;
+    }
+}
+
+class Decorator
+{
+    public function execute()
+    {
+        $result = (new Example)->run();
+        $result = $this->paginate($result);
+        return $result;
+    }
+
+    protected function paginate($result)
+    {
+        //Additional logic
+        return $result;
+    }
+}
+
+class Client
+{
+    public function handle()
+    {
+        return (new Decorator())->execute();
+    }
+}
+
+$result = (new Client())->handle();
+var_dump($result);
