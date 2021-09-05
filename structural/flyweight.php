4ce673f95c67aa4f9c8684913a13427b50f2dc59
1-git <one-git@yandex.com>
2021-09-05 21:40:45 +0300

    Flyweight

---
+++ structural/flyweight.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DesignPattern\Behavioral\Flyweight;
+
+class Player
+{
+    public $color;
+}
+
+class PlayerFactory
+{
+    public static $players;
+
+    public static function getPlayer($type)
+    {
+        if (!isset(self::$players[$type])) {
+            self::$players[$type] = new Player();
+        }
+        return self::$players[$type];
+
+    }
+}
+
+$player1 = PlayerFactory::getPlayer('red');
+$player1->color = 'red';
+
+$player2 = PlayerFactory::getPlayer('red');
+$player2->color = 'yellow';
+
+$player3 = PlayerFactory::getPlayer('blue');
+$player3->color = 'blue';
+
+$player4 = PlayerFactory::getPlayer('blue');
+$player4->color = 'green';
+
+var_dump($player1);
+var_dump($player2);
+var_dump($player3);
+var_dump($player4);
